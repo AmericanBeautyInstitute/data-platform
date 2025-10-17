e446222ac023284db687b1c92e1a6adc3700c60d
andrewwkimm <andrewkimka@gmail.com>
2025-10-17 03:50:41 +0000

    Remove source parameter and add kwargs to method

--- pipelines/extract/extractor.py
+++ pipelines/extract/extractor.py
@@ -13,15 +13,13 @@ class Extractor(ABC):
     def __init__(
         self,
         credentials_file_path: Path | str,
-        source: str,
         config: dict[str, Any] | None = None,
     ) -> None:
         """Initializes the Extractor."""
         self.config = config or {}
         self.credentials_file_path = Path(credentials_file_path)
-        self.source = source
 
     @abstractmethod
-    def extract(self) -> pa.Table:
+    def extract(self, **kwargs) -> pa.Table:
         """Extracts data from the source and returns it in a uniform format."""
         pass
