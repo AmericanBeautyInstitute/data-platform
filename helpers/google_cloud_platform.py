36e5eddfc7a9a6a75cda061e783fce031ee3dffc
andrewwkimm <andrewkimka@gmail.com>
2025-10-20 02:39:48 +0000

    Add authentication helper for Google Ads

--- helpers/google_cloud_platform.py
+++ helpers/google_cloud_platform.py
@@ -2,28 +2,35 @@
 
 from pathlib import Path
 
+from google.ads.googleads.client import GoogleAdsClient
 from google.cloud import storage
 from google.oauth2 import service_account
 from googleapiclient.discovery import Resource, build
 
 
-def authenticate_google_sheets(credentials_file_path: Path | str) -> Resource:
-    """Authenticates and builds a Google Sheets resource."""
-    credentials = service_account.Credentials.from_service_account_file(
-        credentials_file_path,
-        scopes=["https://www.googleapis.com/auth/spreadsheets.readonly"],
-    )
-    google_sheets_credentials = build("sheets", "v4", credentials=credentials)
-    return google_sheets_credentials
+def authenticate_google_ads(credentials_file_path: Path | str) -> GoogleAdsClient:
+    """Authenticates and returns the Google Ads client."""
+    google_ads_client = GoogleAdsClient.load_from_storage(str(credentials_file_path))
+    return google_ads_client
 
 
 def authenticate_google_cloud_storage(
     credentials_file_path: Path | str,
 ) -> storage.Client:
-    """Authenticates and returns Google Cloud Storage client."""
+    """Authenticates and returns the Google Cloud Storage client."""
     credentials = service_account.Credentials.from_service_account_file(
         credentials_file_path,
         scopes=["https://www.googleapis.com/auth/cloud-platform"],
     )
     gcs_client = storage.Client(credentials=credentials, project=credentials.project_id)
     return gcs_client
+
+
+def authenticate_google_sheets(credentials_file_path: Path | str) -> Resource:
+    """Authenticates and returns the Google Sheets resource."""
+    credentials = service_account.Credentials.from_service_account_file(
+        credentials_file_path,
+        scopes=["https://www.googleapis.com/auth/spreadsheets.readonly"],
+    )
+    google_sheets_credentials = build("sheets", "v4", credentials=credentials)
+    return google_sheets_credentials
