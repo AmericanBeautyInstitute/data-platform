aeda7d7ddb8d6b73955fd10e219356ae9bcc4e48
andrewwkimm <andrewkimka@gmail.com>
2025-10-20 02:40:31 +0000

    Specify what type of file the credentials file is

--- helpers/google_cloud_platform.py
+++ helpers/google_cloud_platform.py
@@ -9,7 +9,11 @@ from googleapiclient.discovery import Resource, build
 
 
 def authenticate_google_ads(credentials_file_path: Path | str) -> GoogleAdsClient:
-    """Authenticates and returns the Google Ads client."""
+    """Authenticates and returns the Google Ads client.
+
+    credentials_file_path: Path | str
+        Path to the Google Ads YAML file.
+    """
     google_ads_client = GoogleAdsClient.load_from_storage(str(credentials_file_path))
     return google_ads_client
 
@@ -17,7 +21,11 @@ def authenticate_google_ads(credentials_file_path: Path | str) -> GoogleAdsClien
 def authenticate_google_cloud_storage(
     credentials_file_path: Path | str,
 ) -> storage.Client:
-    """Authenticates and returns the Google Cloud Storage client."""
+    """Authenticates and returns the Google Cloud Storage client.
+
+    credentials_file_path: Path | str
+        Path to the Google Cloud Platform Service Account JSON file.
+    """
     credentials = service_account.Credentials.from_service_account_file(
         credentials_file_path,
         scopes=["https://www.googleapis.com/auth/cloud-platform"],
@@ -27,7 +35,11 @@ def authenticate_google_cloud_storage(
 
 
 def authenticate_google_sheets(credentials_file_path: Path | str) -> Resource:
-    """Authenticates and returns the Google Sheets resource."""
+    """Authenticates and returns the Google Sheets resource.
+
+    credentials_file_path: Path | str
+        Path to the Google Cloud Platform service account JSON file.
+    """
     credentials = service_account.Credentials.from_service_account_file(
         credentials_file_path,
         scopes=["https://www.googleapis.com/auth/spreadsheets.readonly"],
