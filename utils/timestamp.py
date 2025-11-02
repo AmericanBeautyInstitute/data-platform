29f21b6720e0b166670a549dd905718eea5e8aed
andrewwkimm <andrewkimka@gmail.com>
2025-11-02 20:26:55 +0000

    Add true datetime UTC generator

--- utils/timestamp.py
+++ utils/timestamp.py
@@ -3,7 +3,13 @@
 from datetime import UTC, datetime
 
 
-def generate_timestamp() -> str:
-    """Returns the current UTC timestamp in ISO-like format."""
+def generate_file_timestamp() -> str:
+    """Generates a UTC timestamp in ISO-like format for file names."""
     timestamp = datetime.now(UTC).strftime("%Y-%m-%dT%H%M%S")
+    return timestamp
+
+
+def generate_utc_timestamp() -> datetime:
+    """Generates a pure UTC timestamp for "created_at" column."""
+    timestamp = datetime.now(UTC)
     return timestamp
