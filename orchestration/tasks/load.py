a4ad40c1606f364e640caac6d1903a2bb7c60c95
andrewwkimm <andrewkimka@gmail.com>
2025-11-02 08:35:52 +0000

    Pass in a built client instead of the credentials file path (#12)

--- orchestration/tasks/load.py
+++ orchestration/tasks/load.py
@@ -3,6 +3,7 @@
 import pyarrow as pa
 from prefect import get_run_logger, task
 
+from helpers.authenticator import google_cloud_storage_authenticator
 from load.google_cloud_storage_loader import GoogleCloudStorageLoader
 
 
@@ -15,7 +16,9 @@ def load_to_google_cloud_storage(
 ) -> None:
     """Prefect task to load extracted data into Google Cloud Storage."""
     logger = get_run_logger()
-    loader = GoogleCloudStorageLoader(credentials_file_path)
+
+    gcs_client = google_cloud_storage_authenticator(credentials_file_path)
+    loader = GoogleCloudStorageLoader(gcs_client)
 
     logger.info(f"Uploading table to gs://{bucket_name}/{blob_name}")
     loader.load(data=data, bucket_name=bucket_name, blob_name=blob_name)
