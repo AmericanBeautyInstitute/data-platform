2b0622176addd93206624f23598f46f52be1a219
andrewwkimm <andrewkimka@gmail.com>
2025-11-03 19:03:47 +0000

    Use only one timestamp format

--- utils/timestamp.py
+++ utils/timestamp.py
@@ -3,13 +3,7 @@
 from datetime import UTC, datetime
 
 
-def generate_file_timestamp() -> str:
-    """Generates a UTC timestamp in ISO-like format for file names."""
-    timestamp = datetime.now(UTC).strftime("%Y-%m-%dT%H%M%S")
-    return timestamp
-
-
-def generate_utc_timestamp() -> datetime:
+def generate_timestamp() -> datetime:
     """Generates a pure UTC timestamp for "created_at" column."""
     timestamp = datetime.now(UTC)
     return timestamp
