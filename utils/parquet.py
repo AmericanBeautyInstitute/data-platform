59e1336241350a3ce65011920f79edf54fa887de
andrewwkimm <andrewkimka@gmail.com>
2025-11-03 23:19:39 +0000

    Add functions to write partitioned parquets

--- utils/parquet.py
+++ utils/parquet.py
@@ -12,3 +12,20 @@ def table_to_parquet_buffer(table: pa.Table) -> io.BytesIO:
     pq.write_table(table, buffer)
     buffer.seek(0)
     return buffer
+
+
+def write_parquet(table: pa.Table) -> io.BytesIO:
+    """Writes a single PyArrow table to Parquet buffer."""
+    buffer = table_to_parquet_buffer(table)
+    return buffer
+
+
+def write_partitioned_parquet(
+    partitioned_data: dict[str, pa.Table],
+) -> dict[str, io.BytesIO]:
+    """Writes partitioned tables to separate Parquet buffers."""
+    buffers = {
+        partition_key: table_to_parquet_buffer(table)
+        for partition_key, table in partitioned_data.items()
+    }
+    return buffers
