a82176be1e84f38c71655d1eab3913a818870187
andrewwkimm <andrewkimka@gmail.com>
2026-04-06 16:26:21 +0000

    Add retries to Dagster assets

--- assets/ingestion/jobs.py
+++ assets/ingestion/jobs.py
@@ -1,8 +1,14 @@
 """Ingestion layer Dagster job definitions."""
 
-from dagster import AssetSelection, define_asset_job
+from dagster import AssetSelection, Backoff, Jitter, RetryPolicy, define_asset_job
 
 ingestion_job = define_asset_job(
     name="ingestion_job",
     selection=AssetSelection.groups("ingestion"),
+    op_retry_policy=RetryPolicy(
+        max_retries=3,
+        delay=30,
+        backoff=Backoff.EXPONENTIAL,
+        jitter=Jitter.FULL,
+    ),
 )
